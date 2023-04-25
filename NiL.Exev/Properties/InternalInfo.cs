5823512e6705913bbb8928e5b527455643f3876e
Полхович Дмитрий <polhovich.dm@mail.ru>
2023-04-25 13:52:44 +0300

    Build setup

---
+++ NiL.Exev/Properties/InternalInfo.cs
@@ -0,0 +1,5 @@
+internal static class InternalInfo
+{
+    internal const string Version = "1.0.3";
+    internal const string Year = "2023";
+}
--- Utils/buildReleaseMessage.ps1
+++ Utils/buildReleaseMessage.ps1
@@ -1,5 +1,4 @@
-cd ..
-$LAST_TAG=$(git tag|where{$_ -Match "^\d+.\d+.\d+$"})[-1]
+$LAST_TAG=$(echo '';git tag|where{$_ -Match "^\d+.\d+.\d+$"})[-1]
 $URL=$(git remote get-url origin).Replace('.git', '') + "/commit/";
 $COMMITS=$(git cherry $LAST_TAG HEAD)
 foreach ($commit in $COMMITS){ 
