6515f015e3412465f61840408cd6838d744f6e56
Полхович Дмитрий <polhovich.dm@mail.ru>
2023-04-25 13:52:44 +0300

    Build setup

--- Utils/pack.ps1
+++ Utils/pack.ps1
@@ -1,20 +1,20 @@
-cd ..
 $VERSION="$VERSION"
-if ($VERSION -eq "") { $VERSION="2.5" }
+if ($VERSION -eq "") { $VERSION="1.0" }
 echo $(
-rd nil.js\bin -Force -Recurse -erroraction 'silentlycontinue'
-rd nil.js\obj -Force -Recurse -erroraction 'silentlycontinue'
+rd nil.Exev\bin -Force -Recurse -erroraction 'silentlycontinue'
+rd nil.Exev\obj -Force -Recurse -erroraction 'silentlycontinue'
 mkdir nuget -erroraction 'silentlycontinue'
 ) > $null
-$REVISION=$(git rev-list --count origin/develop)
+$REVISION=$(git rev-list --count origin/master)
 echo "VERSION: $VERSION.$REVISION"
-[System.IO.File]::WriteAllText("$(get-location)\\NiL.JS\\Properties\\InternalInfo.cs","internal static class InternalInfo
+[System.IO.File]::WriteAllText("$(get-location)\\NiL.Exev\\Properties\\InternalInfo.cs","internal static class InternalInfo
 {
     internal const string Version = ""$VERSION.$($REVISION)"";
     internal const string Year = ""$(get-date -Format yyyy)"";
 }")
-cd NiL.JS
+cd NiL.Exev
+dotnet run --project ./../Utils/tiny-t4/tiny-t4.csproj --framework net7.0
 dotnet build -c Release -property:VersionPrefix=$VERSION.$($REVISION) -property:SignAssembly=true
 dotnet pack -c Release -property:VersionPrefix=$VERSION.$($REVISION) -property:SignAssembly=true
-mv -Force bin/release/NiL.JS.$VERSION.$($REVISION).nupkg ../nuget/NiL.JS.$VERSION.$($REVISION).nupkg
+mv -Force bin/release/NiL.Exev.$VERSION.$($REVISION).nupkg ../nuget/NiL.Exev.$VERSION.$($REVISION).nupkg
 cd ..
